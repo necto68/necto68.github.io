c89bf3e6ac9543f2f5a5a8995a320ed023c71718
necto68 <necto68necto68@gmail.com>
2015-04-05 07:42:46 +0300

    123

--- WeGo/js/js.js
+++ WeGo/js/js.js
@@ -3,27 +3,20 @@
  */
 $(function(){
    $('.joinUs div span').on("mouseover",function(){
-       /*$('.letter:eq(1), .letter:eq(2)').animate(
-           {height:"99%"
-            paddingTop:"40%"
-           }
-           , 1000);*/
-       //$('.letter').eq(2).css("position","absolute").animate({top:"40%"}, 1000);
        $('.joinUs').stop().animate({top:"-20%"}, 400);
-       //$('.letter').eq(1)
-       //$('.letter').eq(2).animate({top:"40%"}, 1000);
    });
-    $('.joinUs').on('mouseleave', function(){
+   $('.joinUs').on('mouseleave', function(){
         $('.joinUs').stop().animate({top:"-50%"}, 400);
-    })
-    $('.letter').on('mouseover', function(){
+   });
+
+   $('.letter').on('mouseover', function(){
         $(this).siblings('.letter').stop().animate({
           opacity:'0.4'
         }, 400)
-    }).on('mouseleave', function(){
+   }).on('mouseleave', function(){
         $(this).siblings('.letter').stop().animate({
             opacity:'1'
         }, 400)
 
-    })
+   })
 });
