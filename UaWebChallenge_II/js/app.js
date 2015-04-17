210712e039af1a39e4615b4e60a1395c90ad943f
necto68 <necto68necto68@gmail.com>
2015-04-17 14:21:55 +0300

    123

--- UaWebChallenge_II/js/app.js
+++ UaWebChallenge_II/js/app.js
@@ -4,24 +4,11 @@
 (function() {
     var UWCApp = angular.module('UWCApp', [
         'ngRoute',
+        'ngSanitize',
         'UWCAppControllers'
     ]);
 
-
-    UWCApp.controller('MainCtrl', ['$scope', '$http',
-        function ($scope, $http) {
-            if (localStorage.getItem('data')) {
-                $scope.data = angular.fromJson(localStorage.getItem('data'));
-            }
-            else {
-                $http.get('data/data.json').success(function (data) {
-                    $scope.data = data;
-                    localStorage.setItem('data', angular.toJson(data))
-                });
-            }
-        }]);
-
-    angular.module('UWCApp', ['ngRoute']).config(['$routeProvider',
+    UWCApp.config(['$routeProvider',
         function ($routeProvider) {
             $routeProvider.
                 when('/catalogue/:catId', {
--- UaWebChallenge_II/js/controllers.js
+++ UaWebChallenge_II/js/controllers.js
@@ -4,7 +4,7 @@
 
 var UWCAppControllers = angular.module('UWCAppControllers', []);
 
-/* UWCAppControllers.controller('MainCtrl', ['$scope', '$http',
+ UWCAppControllers.controller('MainCtrl', ['$scope', '$http',
     function($scope, $http) {
         if (localStorage.getItem('data')){
             $scope.data = angular.fromJson(localStorage.getItem('data'));
@@ -15,7 +15,20 @@ var UWCAppControllers = angular.module('UWCAppControllers', []);
                 localStorage.setItem('data', angular.toJson(data))
             });
         }
-    }]);*/
+        var catStr = "";
+        $scope.data.Categories.forEach(function(c, i, cat){
+            if((i+1)/4 == Math.floor((i+1)/4) || i == cat.length-1){
+                catStr += '<div class="crow-item"><i id="cr"'+(i+1)+'><img src='+ c.img+' alt="..."></i><span>'+ c.name+'</span></div></div>';
+            }
+            else if((i+1)%4==1 || i == 0 ){
+            catStr += '<div class="catal-row"><div class="crow-item"><i id="cr"'+(i+1)+'><img src='+c.img+' alt="..."></i><span>'+c.name+'</span></div>';
+            }
+            else {
+                catStr += '<div class="crow-item"><i id="cr"'+(i+1)+'><img src='+ c.img+' alt="..."></i><span>'+ c.name+'</span></div>';
+            }
+        });
+        $scope.catStr = catStr;
+    }]);
 
 
 
@@ -28,7 +41,7 @@ UWCAppControllers.controller('CatCtrl', ['$scope', '$routeParams',
         $scope.catId = $routeParams.catId;
     }]);
 
-/*
+
 UWCAppControllers.controller('ProductCtrl', function($scope, $routeParams) {
 
-});*/
+});
